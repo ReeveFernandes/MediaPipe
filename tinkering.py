8c1799828cfbd2ab92ca441b66c8ffa04d99a5af
ArleenMonteiro <111902870+ArleenMonteiro@users.noreply.github.com>
2022-10-23 08:21:50 -0400

    Add files via upload
    New code for main menu using tkinter

--- tinkering.py
+++ tinkering.py
@@ -9,6 +9,9 @@ root.geometry("500x500")
 #Configure the rows and columns
 tkinter.Grid.rowconfigure(root,0,weight=1)
 tkinter.Grid.rowconfigure(root,1,weight=1)
+tkinter.Grid.rowconfigure(root,2,weight=1)
+tkinter.Grid.rowconfigure(root,3,weight=1)
+
 tkinter.Grid.columnconfigure(root,0,weight=1)
 tkinter.Grid.columnconfigure(root,1,weight=1)
 
@@ -16,7 +19,13 @@ tkinter.Grid.columnconfigure(root,1,weight=1)
 
 
 
-#create 4 buttons
+#create name box and label
+EnterName=tkinter.Label(root, text="ENTER YOUR NAME BELOW:")
+EnterName.grid(sticky="nsew")
+Name=tkinter.Entry(root)
+Name.grid(sticky="nsew")
+
+#Enter 4 buttons
 curls=tkinter.Button(root,text="CURLS COUNTER", )
 curls.grid(sticky="nsew")
 push=tkinter.Button(root,text="PUSH UPS COUNTER")
@@ -28,11 +37,13 @@ lunges.grid(sticky="nsew")
 
 
 #Grid the buttons onto the screen
+EnterName.grid(row=0,column=0,columnspan=2)
+Name.grid(row=1,column=0,columnspan=2)
+curls.grid(row=2,column=0)
+push.grid(row=2, column=1)
+squats.grid(row=3,column=0)
+lunges.grid(row=3,column=1)
 
-curls.grid(row=0,column=0)
-push.grid(row=0, column=1)
-squats.grid(row=1,column=0)
-lunges.grid(row=1,column=1)
 
 
 
